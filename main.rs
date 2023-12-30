797f547a8825db3a9ccb8afeefaaeaae817b3e9b
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2023-12-30 00:22:25 -0300

    projeto base com rotas e chamada ao vue funcionando

--- main.rs
+++ main.rs
@@ -2,11 +2,13 @@ use actix_files as fs;
 use actix_web::{App, HttpServer};
 mod routes;
 
+use routes::todo_routes::config;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .configure(routes::todo_routes::config)
+            .configure(config)
             .service(fs::Files::new("/", "frontend/dist").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
--- routes/todo_routes.rs
+++ routes/todo_routes.rs
@@ -1,4 +1,5 @@
-use actix_web::{web, Responder};
+use actix_web::{web, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Item {
@@ -13,13 +14,21 @@ pub fn config(cfg: &mut web::ServiceConfig) {
 }
 
 async fn get_items() -> impl Responder {
-    // Implementação para obter itens
+    // Simulando a obtenção de itens
+    let items: Vec<Item> = vec![
+        Item { id: 1, name: "Item 1".to_string() },
+        Item { id: 2, name: "Item 2".to_string() },
+    ];
+
+    // Retorna os itens como JSON
+    HttpResponse::Ok().json(items)
 }
 
-async fn add_item(item: web::Json<Item>) -> impl Responder {
-    // Implementação para adicionar um item
+async fn add_item(_item: web::Json<Item>) -> impl Responder {
+    let item: Item = Item { id: 1, name: "Item 1".to_string() };
+    HttpResponse::Ok().json(item)
 }
 
-async fn delete_item(path: web::Path<usize>) -> impl Responder {
-    // Implementação para excluir um item
+async fn delete_item(_path: web::Path<usize>) -> impl Responder {
+    HttpResponse::Ok().json(true)
 }
